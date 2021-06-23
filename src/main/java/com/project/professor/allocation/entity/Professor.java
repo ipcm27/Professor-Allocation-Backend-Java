126e019025dbda55308dcd2806efdb2acd9d57b7
Igor <igorpcmiranda@gmail.com>
2021-06-23 20:03:05 -0300

    Add Entity professor pois excluis em querer

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,70 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = false)
+	private String name;
+
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	
+	@ManyToOne (optional = false)
+	private Department department;
+
+	public Professor() {
+
+	}
+
+	public Professor(Long id, String name, String cpf, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+}
+
